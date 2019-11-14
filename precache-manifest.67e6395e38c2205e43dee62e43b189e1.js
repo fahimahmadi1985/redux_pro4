32c63593f468c484bed546a153eae3240d186979
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-14 12:28:25 +0100

    Updates

---
+++ precache-manifest.67e6395e38c2205e43dee62e43b189e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b260e1948b1ef4fb635c0018fd3d288e",
+    "url": "/redux_pro4/index.html"
+  },
+  {
+    "revision": "1395f5689c8cd8604dbc",
+    "url": "/redux_pro4/static/js/2.8af5c5da.chunk.js"
+  },
+  {
+    "revision": "842b4fa8d8869e051c47",
+    "url": "/redux_pro4/static/js/main.3eea4e3d.chunk.js"
+  },
+  {
+    "revision": "36995943cf1c413c377e",
+    "url": "/redux_pro4/static/js/runtime-main.48672787.js"
+  }
+]);
